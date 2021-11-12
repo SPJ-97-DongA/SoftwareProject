7f02459ce36c2dc3ac7342adf475b573eb64f64f
강우혁 <kwh909@naver.com>
2021-11-12 18:44:48 +0900

    Initial commit

---
+++ app/src/main/java/com/example/qrtest/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.qrtest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    String point = "10";
+    int sum;
+    //view Objects
+    private Button buttonScan;
+    private TextView textViewPoint, textViewResult;
+    //qr code scanner object
+    private IntentIntegrator qrScan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //View Objects
+        buttonScan = (Button) findViewById(R.id.buttonScan);
+        textViewPoint = (TextView) findViewById(R.id.textViewPoint);
+        textViewResult = (TextView)  findViewById(R.id.textViewResult);
+        textViewPoint.setText(point);
+        //intializing scan object
+        qrScan = new IntentIntegrator(this);
+
+        //button onClick
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //scan option
+                qrScan.setPrompt("Scanning...");
+                //qrScan.setOrientationLocked(false);
+                qrScan.initiateScan();
+            }
+        });
+    }
+
+    //Getting the scan results
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            //qrcode 가 없으면
+            if (result.getContents() == null) {
+                Toast.makeText(MainActivity.this, "취소!", Toast.LENGTH_SHORT).show();
+            } else {
+                //qrcode 결과가 있으면
+                Toast.makeText(MainActivity.this, "스캔완료!", Toast.LENGTH_SHORT).show();
+                try {
+                    //data를 json으로 변환
+                    JSONObject obj = new JSONObject(result.getContents());
+                    sum = Integer.valueOf(obj.getString("point"))+Integer.valueOf(point);
+                    point = Integer.toString(sum);
+                    textViewPoint.setText(point);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    //Toast.makeText(MainActivity.this, result.getContents(), Toast.LENGTH_LONG).show();
+                    textViewResult.setText(result.getContents());
+                }
+            }
+
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
