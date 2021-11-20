e8ac38cb5cad6140860573eebbb193c81b4412a7
user <hhyc2@naver.com>
2021-11-20 19:02:42 +0900

    게시글 등록 ui 게시글 클릭 ui

--- app/src/main/java/com/example/project/activity/BoardActivity.java
+++ app/src/main/java/com/example/project/activity/BoardActivity.java
@@ -5,9 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.project.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class BoardActivity extends AppCompatActivity {
-
+    private FloatingActionButton floatingActionButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
